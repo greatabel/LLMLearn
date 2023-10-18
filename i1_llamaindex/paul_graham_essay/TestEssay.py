2c7c869d082560f21527eb38afdcbc708b6edbe1
greatabel <myreceiver2for2github@gmail.com>
2023-10-18 12:05:55 +0800

    add token count

--- i1_llamaindex/paul_graham_essay/TestEssay.py
+++ i1_llamaindex/paul_graham_essay/TestEssay.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[58]:
 
 
 import os
@@ -14,32 +14,80 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
-# In[24]:
+# In[59]:
 
 
 # https://github.com/run-llama/llama_index/blob/main/examples/paul_graham_essay/TestEssay.ipynb
+# print(os.environ["OPENAI_API_KEY"])
 
 
-# In[2]:
+# In[60]:
 
 
 from llama_index import TreeIndex, SimpleDirectoryReader
 from IPython.display import Markdown, display
 
 
-# In[3]:
+# In[61]:
+
+
+# 计数
+
+import tiktoken
+from llama_index.llms import Anthropic
+from llama_index.callbacks import CallbackManager, TokenCountingHandler
+
+from llama_index import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    ServiceContext,
+    set_global_service_context,
+)
+
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+
+# 设置tokenizer和TokenCountingHandler
+tokenizer = tiktoken.encoding_for_model("text-davinci-003").encode
+
+token_counter = TokenCountingHandler(tokenizer=tokenizer)
+callback_manager = CallbackManager([token_counter])
+
+service_context = ServiceContext.from_defaults(callback_manager=callback_manager)
+set_global_service_context(service_context)
+
+
+# In[62]:
 
 
 documents = SimpleDirectoryReader("data").load_data()
 
 
-# In[4]:
+# In[63]:
 
 
 new_index = TreeIndex.from_documents(documents)
 
 
-# In[5]:
+# In[64]:
+
+
+print(
+    "Embedding Tokens: ",
+    token_counter.total_embedding_token_count,
+    "\n",
+    "LLM Prompt Tokens: ",
+    token_counter.prompt_llm_token_count,
+    "\n",
+    "LLM Completion Tokens: ",
+    token_counter.completion_llm_token_count,
+    "\n",
+    "Total LLM Token Count: ",
+    token_counter.total_llm_token_count,
+    "\n",
+)
+
+
+# In[65]:
 
 
 # set Logging to DEBUG for more detailed outputs
@@ -47,34 +95,53 @@ query_engine = new_index.as_query_engine()
 response = query_engine.query("What did the author do growing up?")
 
 
-# In[6]:
+# In[66]:
 
 
 display(Markdown(f"<b>{response}</b>"))
 
 
-# In[7]:
+# In[67]:
 
 
 # set Logging to DEBUG for more detailed outputs
 response = query_engine.query("What did the author do after his time at Y Combinator?")
 
 
-# In[10]:
+# In[68]:
 
 
 display(Markdown(f"<b>{response}</b>"))
 
 
+# In[69]:
+
+
+print(
+    "Embedding Tokens: ",
+    token_counter.total_embedding_token_count,
+    "\n",
+    "LLM Prompt Tokens: ",
+    token_counter.prompt_llm_token_count,
+    "\n",
+    "LLM Completion Tokens: ",
+    token_counter.completion_llm_token_count,
+    "\n",
+    "Total LLM Token Count: ",
+    token_counter.total_llm_token_count,
+    "\n",
+)
+
+
 # # Build Tree Index with a custom Summary Prompt, directly retrieve answer from root node
 
-# In[11]:
+# In[70]:
 
 
 from llama_index.prompts import PromptTemplate
 
 
-# In[13]:
+# In[71]:
 
 
 documents = SimpleDirectoryReader("data").load_data()
@@ -92,7 +159,7 @@ SUMMARY_PROMPT = PromptTemplate(SUMMARY_PROMPT_TMPL)
 index_with_query = TreeIndex.from_documents(documents, summary_template=SUMMARY_PROMPT)
 
 
-# In[16]:
+# In[72]:
 
 
 # directly retrieve response from root nodes instead of traversing tree
@@ -100,7 +167,7 @@ query_engine = index_with_query.as_query_engine(retriever_mode="root")
 response = query_engine.query(query_str)
 
 
-# In[17]:
+# In[73]:
 
 
 display(Markdown(f"<b>{response}</b>"))
@@ -108,14 +175,14 @@ display(Markdown(f"<b>{response}</b>"))
 
 # # Using GPT Keyword Table Index
 
-# In[18]:
+# In[74]:
 
 
 from llama_index import KeywordTableIndex, SimpleDirectoryReader
 from IPython.display import Markdown, display
 
 
-# In[19]:
+# In[ ]:
 
 
 # build keyword index
@@ -123,7 +190,7 @@ documents = SimpleDirectoryReader("data").load_data()
 index = KeywordTableIndex.from_documents(documents)
 
 
-# In[20]:
+# In[ ]:
 
 
 # set Logging to DEBUG for more detailed outputs
@@ -131,7 +198,7 @@ query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do after his time at Y Combinator?")
 
 
-# In[21]:
+# In[ ]:
 
 
 display(Markdown(f"<b>{response}</b>"))
@@ -139,7 +206,7 @@ display(Markdown(f"<b>{response}</b>"))
 
 # # Using GPT List Index
 
-# In[22]:
+# In[ ]:
 
 
 from llama_index import SummaryIndex, SimpleDirectoryReader
@@ -162,10 +229,29 @@ query_engine = index.as_query_engine()
 response = query_engine.query("What did the author do after his time at Y Combinator?")
 
 
-# In[23]:
+# In[ ]:
 
 
 display(Markdown(f"<b>{response}</b>"))
+
+
+# In[ ]:
+
+
+print(
+    "Embedding Tokens: ",
+    token_counter.total_embedding_token_count,
+    "\n",
+    "LLM Prompt Tokens: ",
+    token_counter.prompt_llm_token_count,
+    "\n",
+    "LLM Completion Tokens: ",
+    token_counter.completion_llm_token_count,
+    "\n",
+    "Total LLM Token Count: ",
+    token_counter.total_llm_token_count,
+    "\n",
+)
 
 
 # In[ ]:
