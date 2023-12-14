974a3d73d7bd458f59d42cb9844b652464c2d7e6
greatabel <myreceiver2for2github@gmail.com>
2023-12-14 18:25:43 +0800

    format *.py

--- i1_llamaindex/paul_graham_essay/i1starter.py
+++ i1_llamaindex/paul_graham_essay/i1starter.py
@@ -1,17 +1,22 @@
 # https://docs.llamaindex.ai/en/stable/getting_started/starter_example.html
 import os.path
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
 
 # check if storage already exists
-if (not os.path.exists('./storage')):
-    print('l加载文档并创建索引')
-    documents = SimpleDirectoryReader('data').load_data()
+if not os.path.exists("./storage"):
+    print("l加载文档并创建索引")
+    documents = SimpleDirectoryReader("data").load_data()
     index = VectorStoreIndex.from_documents(documents)
     # store it for later
     index.storage_context.persist()
 else:
-    print('加载现有索引')
-    storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    print("加载现有索引")
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
     index = load_index_from_storage(storage_context)
 
 # either way we can now query the index
@@ -19,5 +24,5 @@ query_engine = index.as_query_engine()
 Q = "What did the author do growing up?"
 response = query_engine.query(Q)
 print(Q)
-print('------'*10)
+print("------" * 10)
 print(response)
--- i1_llamaindex/paul_graham_essay/i2customization.py
+++ i1_llamaindex/paul_graham_essay/i2customization.py
@@ -1,20 +1,24 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
 
-print('我想将我的文件解析成较小的块')
+print("我想将我的文件解析成较小的块")
 from llama_index import ServiceContext
+
 service_context = ServiceContext.from_defaults(chunk_size=1000)
 
 
-print('我想使用一个不同的向量存储库')
+print("我想使用一个不同的向量存储库")
 # pip3 install chromadb==0.4.0
 import chromadb
 from llama_index.vector_stores import ChromaVectorStore
 from llama_index import StorageContext
-chroma_client = chromadb.PersistentClient()
-print('chroma_client.list_collections()=', chroma_client.list_collections())
 
-if "quickstart" not in [collection.name for collection in chroma_client.list_collections()]:
+chroma_client = chromadb.PersistentClient()
+print("chroma_client.list_collections()=", chroma_client.list_collections())
+
+if "quickstart" not in [
+    collection.name for collection in chroma_client.list_collections()
+]:
     chroma_collection = chroma_client.create_collection("quickstart")
 else:
     chroma_collection = chroma_client.get_collection("quickstart")
@@ -23,8 +27,7 @@ vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
 
-
-documents = SimpleDirectoryReader('data').load_data()
+documents = SimpleDirectoryReader("data").load_data()
 # index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
 query_engine = index.as_query_engine(similarity_top_k=3)
@@ -34,12 +37,12 @@ query_engine = index.as_query_engine()
 Q = "What did the author do growing up?"
 response = query_engine.query(Q)
 print(Q)
-print('------'*10)
+print("------" * 10)
 print(response)
 
-print('#'*20)
+print("#" * 20)
 for i, node_with_score in enumerate(response.source_nodes, 1):
     print(f"Top {i} Option:")
     print(node_with_score.node.text)  # 打印节点的文本内容
     print("Score:", node_with_score.score)  # 打印节点的得分
-    print('#'*20)
+    print("#" * 20)
